fca68b324ab72c031db4ec3324379bb2bda828b5
zRosies <gustavo.enrollment@gmail.com>
2023-04-17 16:55:27 -0300

    asdad

---
+++ w1_debugging.js
@@ -0,0 +1,19 @@
+// const pi = 3.14;
+// var radius = 3;
+// let area = 0;
+// area = radius * radius * pi;
+// console.log("Area1:", area);
+// radius = 4;
+// area = radius * radius * pi;
+// console.log("Area2:", area);
+
+
+function circleArea(radius) {
+    const PI = 3.14;
+    area = radius * PI;
+    return area
+}
+area = circleArea(3);
+console.log(area);
+area = circleArea(6);
+console.log(area)
