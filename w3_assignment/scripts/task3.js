f6ce885b253ca23fb4f7eb2ef93e01ed3cde37aa
zRosies <gustavo.enrollment@gmail.com>
2023-05-02 21:22:58 -0300

    changes

--- w3_assignment/scripts/task3.js
+++ w3_assignment/scripts/task3.js
@@ -16,7 +16,7 @@ function addNumbers(){
 	let addend1 = Number(document.querySelector('#addend1').value);
 	let addend2 = Number(document.querySelector('#addend2').value);
 	let sum = add(addend1,addend2)
-    console.log(sum)
+    // console.log(sum)
     return document.querySelector('#sum').value=sum;
     
 	
@@ -83,7 +83,7 @@ const date = new Date();
 
 // Step 2: Declare a variable to hold the current year
 let year;
-
+// 
 // Step 3: Using the variable declared in Step 1, call the built-in getFullYear() method/function and assign it to the variable declared in Step 2
 year=date.getFullYear()
 // Step 4: Assign the current year variable to an HTML form element with an ID of year
@@ -94,16 +94,56 @@ document.querySelector('#year').textContent=year;
 
 // Step 1: Declare and instantiate an array variable to hold the numbers 1 through 25
 
-let array=[]
+const array=[]
+let i=1
+for (i; i < 26; i++){
+    array.push(i)
+}
+// console.log(array)
 
 // Step 2: Assign the value of the array variable to the HTML element with an ID of "array"
+document.querySelector('#array').textContent=array.join(', ')
 
-// Step 3: Use the filter array method to find all of the odd numbers of the array variable and assign the reult to the HTML element with an ID of "odds" ( hint: % (modulus operartor) )
+// Step 3: Use the filter array method to find all of the odd numbers of the array variable and assign the result to the HTML element with an ID of "odds" ( hint: % (modulus operartor) )
+const oddNumbers=array.filter(function (number){    
+    if (number%2 !== 0){
+        return number;
+        // console.log(number)   
+        // odd.push(number)    
+    } 
+   
+})
+document.querySelector("#odds").innerHTML=oddNumbers.join(', ');
 
 // Step 4: Use the filter array method to find all of the even numbers of the array variable and assign the result to the HTML element with an ID of "evens"
+const evenNumbers=array.filter(function(number){
+    if(number%2===0){
+        return number
+    }
+})
+
+document.querySelector("#evens").innerHTML=evenNumbers.join(', ')
+
 
 // Step 5: Use the reduce array method to sum the array variable elements and assign the result to the HTML element with an ID of "sumOfArray"
+//Reduce functions requires two paramenters an accumulator and a current value;
+let accumulator=0;
+const sum=array.reduce((accumulator, num)=> accumulator+=num);
+
+
+document.querySelector('#sumOfArray').innerHTML=sum;
 
 // Step 6: Use the map array method to multiple each element in the array variable by 2 and assign the result to the HTML element with an ID of "multiplied"
 
+const multiple=array.map(function(number){
+    if (number){
+        return number*=2
+    }
+})
+
+document.querySelector('#multiplied').innerHTML=multiple.join(', ')
 // Step 7: Use the map and reduce array methods to sum the array elements after multiplying each element by two.  Assign the result to the HTML element with an ID of "sumOfMultiplied"
+
+const sumar=multiple.reduce((accummulator,number)=>accummulator+=number);
+
+document.querySelector("#sumOfMultiplied").innerHTML=sumar;
